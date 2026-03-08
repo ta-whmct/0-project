ebb8551c5644c8928735ccb98088dc8ed9480795
ta-whmct <p.tatyana.andreevna@gmail.com>
2026-03-08 19:57:15 +0400

    configure interfaces for bunker

---
+++ app/application/interfaces/bunker.py
@@ -0,0 +1,15 @@
+from typing import Protocol
+
+from app.domain.entities import Bunker
+
+
+class BunkerSaver(Protocol):
+    async def save(self, bunker: Bunker) -> None: ...
+
+
+class BunkerReader(Protocol):
+    async def read_by_uuid(self, uuid: str) -> Bunker | None: ...
+
+
+class UsersReader(Protocol):
+    async def read(self) -> list[Bunker]: ...
