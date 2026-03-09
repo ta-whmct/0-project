93cea31a4fa7e707a50f83ecda4f149a5ed69245
ta-whmct <p.tatyana.andreevna@gmail.com>
2026-03-09 11:12:04 +0400

    add bunker service

---
+++ app/infra/gateways/bunker.py
@@ -0,0 +1,37 @@
+from uuid import UUID
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.application.interfaces.bunker import BunkerReader, BunkerSaver
+from app.domain.entities import Bunker
+from app.infra import models
+
+
+class BunkerGateway(BunkerReader, BunkerSaver):
+    def __init__(self, session: AsyncSession) -> None:
+        self._session = session
+
+    async def read_by_uuid(self, uuid: UUID) -> Bunker | None:
+        bunker = (
+            await self._session.execute(select(models.Bunker).filter_by(id=uuid))
+        ).scalar_one_or_none()
+        if bunker is None:
+            return None
+        return Bunker(
+            id=bunker.id,
+            max_volume=bunker.max_volume,
+            current_volume=bunker.current_volume,
+            pre_close_value=bunker.pre_close_value,
+            product_type=bunker.product_type,
+        )
+
+    async def save(self, bunker: Bunker) -> None:
+        bunker_model = models.Bunker(
+            id=bunker.id,
+            max_volume=bunker.max_volume,
+            current_volume=bunker.current_volume,
+            pre_close_value=bunker.pre_close_value,
+            product_type=bunker.product_type,
+        )
+        self._session.add(bunker_model)
